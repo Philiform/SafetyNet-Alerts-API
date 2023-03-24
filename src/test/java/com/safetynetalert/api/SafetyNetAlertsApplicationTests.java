7e3625a63755c0390b00fb53cea973aefe581cc4
Philippe Pernet <philip.pernet@free.fr>
2023-03-24 11:13:09 +0100

    Commit initial

---
+++ src/test/java/com/safetynetalert/api/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynetalert.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
